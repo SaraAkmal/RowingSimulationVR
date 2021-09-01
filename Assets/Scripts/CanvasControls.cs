0f468fad3b2a732d21ea9688ed7b73cc70c0a957
Sara Akmal <sarahak.676@gmail.com>
2021-09-01 16:48:16 +0200

    Cross Fade Menu

--- Assets/Scripts/CanvasControls.cs
+++ Assets/Scripts/CanvasControls.cs
@@ -1,15 +1,26 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class CanvasControls : MonoBehaviour
 {
+    [SerializeField] private Animator transition;
+    [SerializeField] private float transitionTime = 1;
+
     public void ChangeScene()
     {
-        SceneManager.LoadScene(1, LoadSceneMode.Single);
+        StartCoroutine(LoadLevel(1));
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    private IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.enabled = true;
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex);
     }
 }
